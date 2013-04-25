895958d4c1d1d5bdb7bff360f94805f54cc24f20
WesleySmits <wesleysmits@gmail.com>
2013-04-25 14:07:46 +0200

    changed game layout and design and removed capital letters from url

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -3,3 +3,13 @@ var socket = io.connect('http://localhost');
 socket.on('socket', function(data){
 	console.log(data);
 });
+
+$(document).ready(function() {
+	var card;
+	$('div.card').on('click', function(){
+		card = $(this).find('img').attr('src');
+		$('section.info').html('<img src=""/>');
+		$('section.info').find('img').attr('src', card);
+		$('section.info').show();
+	})
+});
